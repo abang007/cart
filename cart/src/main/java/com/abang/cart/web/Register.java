869563dd93263c3e089e178889beb6d19b99b3b7
abang007 <tt850955100@qq.com>
2019-09-11 00:23:41 +0800

    前后端json数据传输

--- cart/src/main/java/com/abang/cart/web/Register.java
+++ cart/src/main/java/com/abang/cart/web/Register.java
@@ -3,13 +3,10 @@ package com.abang.cart.web;
 import cn.hutool.json.JSONObject;
 import com.abang.cart.bean.User;
 import com.abang.cart.dao.UserDao;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -17,47 +14,41 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @Controller
 public class Register {
-
     @Autowired
     User user;
     @Autowired
     UserDao userDao;
-    @RequestMapping(value = "/register",method = POST)
-    public void register(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        String name = request.getParameter("name");
-        String passwd = request.getParameter("passwd");
-        System.out.println("name:" + name);
-        System.out.println("passwd:"+passwd);
-        JSONObject jsonObject = new JSONObject();
+    //接受用户注册请求
+    @RequestMapping(value = "/register", method = POST)
+    public void register(@RequestBody JSONObject json,HttpServletResponse response) throws IOException {
+//        System.out.println(json.toString());
+        String name = (String) json.get("name");
+        String passwd = (String) json.get("passwd");
+        System.out.println("name:" + name+","+"passwd:"+passwd);
+        JSONObject object = new JSONObject();  //创建Json对象
+        response.setCharacterEncoding("UTF-8");
+        //用户名及密码如果为空
+        if (name == null || passwd == null || ("").equals(name) || ("").equals(passwd)) {
 
-        if ((name == null || ("").equals(name) || ((passwd == null) || ("").equals(passwd)))) {
-            System.out.println("请填写完整的注册信息！");
-            String str = "请填写完整的注册信息！";
-            jsonObject.put("msg",str);
-            String obj = jsonObject.toString();
-            response.getWriter().write(obj);
-            System.out.println(obj);
-
+            object.put("msg", "用户名或密码不能为空！！！");   //设置Json对象的属性
+            System.out.println(object.toString());
+            response.getWriter().write(String.valueOf(object));
+            //用户名及密码如果不为空
         } else {
-            String dataPasswd = userDao.getUser(name);
-            //若用户不存在，则进行注册
-            if (null == dataPasswd) {
+            String dataPasswd = userDao.getUserName(name);
+            System.out.println("dataUser："+dataPasswd);
+            //查不到该用户
+            if (dataPasswd == null) {
                 user.setName(name);
                 user.setPasswd(passwd);
-                System.out.println(user.toString());
                 userDao.insertUser(user);
-                System.out.println("注册成功！");
-            } else {//若用户存在则表示用户已被注册
-                String msg = "该用户已被注册";
-                jsonObject.put("msg",msg);
-                response.getWriter().write(jsonObject.toString());
-                return;
+                object.put("msg", "注册成功！！！");
+                response.getWriter().write(String.valueOf(object));
+            } else {
+                object.put("msg", "注册的用户已存在！！！");
+                response.getWriter().write(String.valueOf(object));
             }
         }
-        String msg = "注册成功";
-        jsonObject.put("msg",msg);
-        response.getWriter().write(jsonObject.toString());
-        return;
     }
 
 }
