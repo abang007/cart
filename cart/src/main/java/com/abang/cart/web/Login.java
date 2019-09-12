1841425b0e8044d8ca6a59f854918eeee2894f60
abang007 <tt850955100@qq.com>
2019-09-12 22:33:45 +0800

    配置日志

--- cart/src/main/java/com/abang/cart/web/Login.java
+++ cart/src/main/java/com/abang/cart/web/Login.java
@@ -3,15 +3,12 @@ package com.abang.cart.web;
 import cn.hutool.json.JSONObject;
 import com.abang.cart.bean.User;
 import com.abang.cart.dao.UserDao;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
@@ -21,20 +18,20 @@ public class Login {
     User user;
     @Autowired
     UserDao userDao;
-
     @RequestMapping(value = "/login", method = RequestMethod.POST)
     public void login(@RequestBody JSONObject data, HttpServletResponse response) throws IOException {
-        System.out.println(data);
+        final Logger logger = Logger.getLogger(Login.class);
+        logger.info("传入的数据:"+data);
         String name = (String) data.get("user");
         String passwd = (String) data.get("passwd");
         user.setName(name);
         user.setPasswd(passwd);
-        System.out.println(user.toString());
+
         JSONObject object = new JSONObject();
         response.setCharacterEncoding("utf-8");
         //判断是否是用户注册
         //判断用户登陆
-        System.out.println("name:" + name);
+        logger.debug(name);
         if (name == null || name.equals("")) {
             object.put("msg", "用户不存在，请点击注册！");
             response.getWriter().write(object.toString());
@@ -42,7 +39,7 @@ public class Login {
             return;
         }
         String dataPasswd = userDao.getUser(name);
-        System.out.println("dataPasswd:"+dataPasswd);
+        System.out.println("dataPasswd:" + dataPasswd);
         if (dataPasswd == null || !dataPasswd.equals(passwd)) {
             object.put("msg", "用户或者密码输入错误");
             response.getWriter().write(object.toString());
